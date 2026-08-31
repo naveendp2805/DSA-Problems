69971226541dc28cbe73ecb8116ddd292e97f4aa
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-08-31 12:19:18 +0530

    feat(medium): add longest-consecutive-sequence solution [Java]

---
+++ leetcode/medium/0128-longest-consecutive-sequence/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums)
+            set.add(num);
+
+        int res = 0;
+
+        for(int num : set)
+        {
+            if(!set.contains(num - 1))
+            {
+                int len = 1;
+
+                while(set.contains(num + len))
+                    len++;
+
+                res = Math.max(res, len);
+            }
+        }
+
+        return res;
+    }
+}
