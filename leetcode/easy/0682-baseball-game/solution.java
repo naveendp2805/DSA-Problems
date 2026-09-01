8adce5fb93d8e1fcdee9e8a28f41e46f1f6ba331
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-01 20:54:03 +0530

    feat(easy): add baseball-game solution [Java]

---
+++ leetcode/easy/0682-baseball-game/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int res = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(String op : operations)
+        {
+            if(op.equals("C")) stack.pop();
+            else if(op.equals("D")) stack.push(stack.peek()*2);
+            else if(op.equals("+"))
+            {
+                int first = stack.pop();
+                int second = stack.peek();
+
+                stack.push(first);
+                stack.push(first + second);
+            }else
+                stack.push(Integer.parseInt(op));
+        }
+
+        while(!stack.isEmpty()) res += stack.pop();
+
+        return res;
+    }
+}
