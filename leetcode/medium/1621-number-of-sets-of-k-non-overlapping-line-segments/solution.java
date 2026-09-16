37dbdb6adcace4114754a9f1976a8f6e3071f872
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-16 11:22:09 +0530

    feat(medium): add number-of-sets-of-k-non-overlapping-line-segments solution [Java]

---
+++ leetcode/medium/1621-number-of-sets-of-k-non-overlapping-line-segments/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+        int mod = 1000000007;
+        long[][] dp = new long[n][k+1];
+
+        for(int i=0; i<n; i++)
+            dp[i][0] = 1;
+
+        for(int j=1; j<=k; j++)
+        {
+            long sum = 0;
+            for(int i=1; i<n; i++)
+            {
+                sum = (sum + dp[i-1][j-1]) % mod;
+
+                dp[i][j] = (dp[i-1][j] + sum) % mod;
+            }
+        }
+
+        return (int) dp[n-1][k];
+    }
+}
