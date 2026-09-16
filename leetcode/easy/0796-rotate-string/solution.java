1fe4617a3e0674532beb0a239f4c01ec8b359c58
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-16 18:53:35 +0530

    feat(easy): add rotate-string solution [Java]

---
+++ leetcode/easy/0796-rotate-string/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+
+        int n = s.length();
+
+        for(int i=0; i<n; i++)
+        {
+            String rotated = s.substring(i) + s.substring(0, i);
+
+            if(rotated.equals(goal))
+                return true;
+        }
+
+        return false;
+    }
+}
