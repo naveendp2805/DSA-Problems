41a67749bfd8626801d6a09b3163c5c46d053370
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-16 11:45:18 +0530

    feat(medium): add next-permutation solution [Java]

---
+++ leetcode/medium/0031-next-permutation/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        int i = n-2;
+        while(i >= 0 && nums[i] >= nums[i+1])
+            i--;
+
+        if(i >= 0)
+        {
+            int j = n-1;
+            while(nums[i] >= nums[j])
+                j--;
+
+            swap(nums, i, j);
+        }
+
+        reverseArray(nums, i+1, n-1);
+    }
+
+    private static void reverseArray(int[] nums, int i, int j)
+    {
+        while(i < j)
+            swap(nums, i++, j--);
+    }
+
+    private static void swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
