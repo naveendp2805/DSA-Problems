cd99a5591fccf7ea58e775838555f08831b9e21b
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-16 12:36:11 +0530

    feat(easy): add best-time-to-buy-and-sell-stock solution [Java]

--- leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.java
+++ leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int max = -1, maxProfit = -1;
+        int max = 0, maxProfit = 0;
 
         for(int i=n-1; i>=0; i--)
         {
