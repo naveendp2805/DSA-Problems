53ca766b13e7baea95ddba39f4c321b99b2303c2
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-21 21:41:45 +0530

    feat(medium): add find-x-value-of-array-i solution [Java]

---
+++ leetcode/medium/3524-find-x-value-of-array-i/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] res = new long[k];
+        int[] freq = new int[k];
+
+        for (int n : nums) 
+        {
+            n %= k;
+
+            int[] curr = new int[k];
+            curr[n] = 1;
+
+            for (int x = 0; x < k; x++)
+                curr[x * n % k] += freq[x];
+
+            freq = curr;
+            for (int x = 0; x < k; x++)
+                res[x] += freq[x];
+        }
+
+        return res;
+    }
+}
