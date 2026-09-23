50a1a5df5b4ba749ac952b110c3faa8c94bba36f
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-23 20:57:34 +0530

    feat(medium): add word-search solution [Java]

---
+++ leetcode/medium/0079-word-search/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(check(board, visited, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean check(char[][] board, boolean[][] visited, String word, int i, int j, int k)
+    {
+        if(k == word.length())
+            return true;
+
+        if(i < 0 || i == board.length || j < 0 || j == board[0].length || visited[i][j] || board[i][j] != word.charAt(k))
+            return false;
+
+        visited[i][j] = true;
+
+        if(check(board, visited, word, i-1, j, k+1) || check(board, visited, word, i+1, j, k+1) ||
+            check(board, visited, word, i, j-1, k+1) || check(board, visited, word, i, j+1, k+1))
+            return true;
+
+        visited[i][j] = false;
+
+        return false;
+    }
+}
