ca18e78126f6c4aa0ea9f6ad4c0af6bf7e7dae30
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-24 19:15:47 +0530

    feat(medium): add max-number-of-k-sum-pairs solution [Java]

---
+++ leetcode/medium/1679-max-number-of-k-sum-pairs/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int i=0, j=nums.length-1;
+        int res = 0;
+
+        while(i < j)
+        {
+            int sum = nums[i] + nums[j];
+
+            if(sum == k)
+            {
+                res++;
+                i++;
+                j--;
+            }
+            else if(sum < k) i++;
+            else j--;
+        }
+
+        return res;
+    }
+}
