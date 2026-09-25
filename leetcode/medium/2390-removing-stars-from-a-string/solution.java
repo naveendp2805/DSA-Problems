c67d7e477df3b28d802f942d5f8af5fc7dbb0d85
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-25 19:54:08 +0530

    feat(medium): add removing-stars-from-a-string solution [Java]

---
+++ leetcode/medium/2390-removing-stars-from-a-string/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray())
+        {
+            if(ch == '*') stack.pop();
+            else stack.push(ch);
+        }
+
+        String res = "";
+
+        while(!stack.isEmpty())
+            res = String.valueOf(stack.pop()) + res; 
+
+        return res;
+    }
+}
