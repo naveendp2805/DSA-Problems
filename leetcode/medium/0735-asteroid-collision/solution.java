4f386a00658d8280df0bf6758e2aef9f76cf68fb
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-25 20:12:41 +0530

    feat(medium): add asteroid-collision solution [Java]

---
+++ leetcode/medium/0735-asteroid-collision/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int asteroid : asteroids)
+        {
+            if(asteroid > 0) {
+                stack.push(asteroid);
+            } else {
+                while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -asteroid)
+                    stack.pop();
+
+                if(stack.isEmpty() || stack.peek() < 0)
+                    stack.push(asteroid);
+                    
+                if(stack.peek() == -asteroid)
+                    stack.pop();
+            }
+        }
+
+        int n = stack.size(), i = n-1;
+        int[] res = new int[n];
+
+        while(!stack.isEmpty())
+            res[i--] = stack.pop();
+
+        return res;
+    } 
+}
