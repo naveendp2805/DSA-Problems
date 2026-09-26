c70fae33e64983f9ea53617fa6bb7be5cfc1e231
Naveen Durga Prasad Eddum <naveendp28122005@gmail.com>
2026-09-26 11:38:46 +0530

    feat(medium): add SEGFAULT solution [Java]

---
+++ codechef/medium/SEGFAULT/solution.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            
+            int[][] arr = new int[n][2];
+            
+            for(int i=0; i<n; i++)
+            {
+                arr[i][0] = sc.nextInt();
+                arr[i][1] = sc.nextInt();
+            }
+            
+            List<Integer> res = findThieves(arr, n);
+            
+            System.out.println(res.size());
+            
+            for(int ele : res)
+                System.out.println(ele);
+        }
+	}
+	
+	private static List<Integer> findThieves(int[][] arr, int n)
+	{
+	    List<Integer> res = new ArrayList<>();
+	    int[] count = new int[n];
+	    
+	    for(int i=0; i<n; i++)
+	    {
+	        int l = arr[i][0], r = arr[i][1];
+	        
+	        for(int j=l-1; j<r; j++)
+	            count[j]++;
+	    }
+	    
+	    int max = 0;
+	    
+	    for(int i=0; i<n; i++)
+	    {
+	        max = Math.max(max, count[i]);
+	    }
+	    
+	    for(int i=0; i<n; i++)
+	    {
+	        if(count[i] == max)
+	            res.add(i+1);
+	    }
+	    
+	    return res;
+	}
+}
